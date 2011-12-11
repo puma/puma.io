c2e24ca89b6b55733abf386ac25bef747f1a15c0
Jordan Isip <jordan@tanga.com>
2011-12-10 21:09:33 -0800

    Getting started with Puma on Jekyll

---
+++ _plugins/haml_converter.rb
@@ -0,0 +1,39 @@
+module Jekyll
+  require 'haml'
+  class HamlConverter < Converter
+    safe true
+    priority :low
+
+    def matches(ext)
+      ext =~ /haml/i
+    end
+
+    def output_ext(ext)
+      ".html"
+    end
+
+    def convert(content)
+      engine = Haml::Engine.new(content)
+      engine.render
+    end
+  end
+
+  require 'sass'
+  class SassConverter < Converter
+    safe true
+    priority :low
+
+     def matches(ext)
+      ext =~ /sass/i
+    end
+
+    def output_ext(ext)
+      ".css"
+    end
+
+    def convert(content)
+      engine = Sass::Engine.new(content)
+      engine.render
+    end
+  end
+end
