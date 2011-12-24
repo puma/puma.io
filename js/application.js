a39f1f353304be870317ad743a4a3e5b6a23f918
Jordan Isip <jordan@tanga.com>
2011-12-24 15:54:39 -0800

    Creating line graph for memory consumption (using fake data)

---
+++ js/application.js
@@ -0,0 +1,28 @@
+---
+---
+
+$(function() {
+  $(window).resize(function(){
+    // Redraw visualization on resize to recalcuate widths
+    $('.visualize').remove();
+
+    var dataTable = $('#memory-usage-comparision table');
+    var visualizationWidth = dataTable.parent().width();
+    var visualizationColors = [];
+
+    // Grab colors from Speed Comparison chart (defined as SASS Variables)
+    $('#speed-comparison ul li').each(function(){
+      visualizationColors.push($(this).css('background-color'));
+    });
+
+    dataTable.visualize({
+      width:visualizationWidth,
+      type:'line',
+      parseDirection:'y',
+      colors: visualizationColors
+    });
+
+    dataTable.hide();
+
+  }).resize();
+});
