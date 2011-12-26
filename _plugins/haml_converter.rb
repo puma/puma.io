a9dc0ce59c4f7274a3d2845a0d98833b73d82256
Jordan Isip <jordan@tanga.com>
2011-12-25 21:23:25 -0800

    Getting rid of unnecessary haml plugin

--- _plugins/haml_converter.rb
+++ _plugins/haml_converter.rb
@@ -1,29 +1,10 @@
 module Jekyll
-  require 'haml'
-  class HamlConverter < Converter
-    safe true
-    priority :low
-
-    def matches(ext)
-      ext =~ /haml/i
-    end
-
-    def output_ext(ext)
-      ".html"
-    end
-
-    def convert(content)
-      engine = Haml::Engine.new(content)
-      engine.render
-    end
-  end
-
   require 'sass'
   class SassConverter < Converter
     safe true
     priority :low
 
-     def matches(ext)
+    def matches(ext)
       ext =~ /sass/i
     end
 
